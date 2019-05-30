8919be29565baf38c0a7f0f5077b945f17dd6b36
stasiazt <stasiazt@gmail.com>
2019-05-30 20:40:22 +0300

    Add test2

--- test/org/itstep/model/CircleTest.java
+++ test/org/itstep/model/CircleTest.java
@@ -1,5 +1,6 @@
 package org.itstep.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,5 +9,9 @@ class CircleTest {
 
     @Test
     void getArea() {
+        Circle circle = new Circle();
+        circle.setRadius(10);
+       double testData = circle.getArea();
+       Assertions.assertEquals(314.1592, testData, 0.0001);
     }
 }
--- test/org/itstep/model/TriangleTest.java
+++ test/org/itstep/model/TriangleTest.java
@@ -12,9 +12,5 @@ class TriangleTest {
         triangle.setBase(10);
         triangle.setHeigth(16);
         Assertions.assertEquals(80, triangle.getArea());
-//        Circle circle = new Circle();
-//        circle.setRadius(10);
-//        double testData = circle.getArea();
-//        Assertions.assertEquals(314.1592, testData, 0.0001);
     }
 }
